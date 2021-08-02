66ed1cf5bf97a9d6b56cbee0c9449108ae0031a5
sunshineplan <sunshineplan@users.noreply.github.com>
2021-08-02 19:35:26 +0800

    workers: rewrite some functions

--- workers/workers.go
+++ workers/workers.go
@@ -6,129 +6,136 @@ import (
 	"runtime"
 )
 
-var defaultWorkers = &Workers{Max: runtime.NumCPU() * 2}
+// NumCPU returns the number of logical CPUs usable by the current process.
+var NumCPU = runtime.NumCPU
 
-// Workers can run jobs concurrently with limit
+var defaultWorkers = &Workers{limit: NumCPU()}
+
+// Workers can run jobs concurrently with limit.
 type Workers struct {
-	Max int
+	limit int
 }
 
-// New creates a new Workers with max limit.
-func New(max int) *Workers {
-	return &Workers{Max: max}
+// New creates a new Workers with the limit.
+func New(n int) *Workers {
+	return &Workers{limit: n}
 }
 
-// SetMax sets default workers max limit.
-func SetMax(max int) {
-	defaultWorkers.Max = max
+// SetLimit sets default workers limit.
+func SetLimit(n int) {
+	defaultWorkers.limit = n
 }
 
-// Slice runs the slice without limit.
-func Slice(Slice interface{}, Runner func(int, interface{})) error {
-	return runSlice(0, Slice, Runner)
+// Slice runs the slice on default workers.
+func Slice(i interface{}, f func(int, interface{})) error {
+	return defaultWorkers.Slice(i, f)
 }
 
-// Map runs the map without limit.
-func Map(Map interface{}, Runner func(interface{}, interface{})) error {
-	return runMap(0, Map, Runner)
+// Map runs the map on default workers.
+func Map(i interface{}, f func(interface{}, interface{})) error {
+	return defaultWorkers.Map(i, f)
 }
 
-// Range runs the range without limit.
-func Range(Start, End int, Runner func(int)) error {
-	return runRange(0, Start, End, Runner)
-}
-
-// DefaultSlice runs the slice on default workers.
-func DefaultSlice(Slice interface{}, Runner func(int, interface{})) error {
-	return defaultWorkers.Slice(Slice, Runner)
-}
-
-// DefaultMap runs the map on default workers.
-func DefaultMap(Map interface{}, Runner func(interface{}, interface{})) error {
-	return defaultWorkers.Map(Map, Runner)
-}
-
-// DefaultRange runs the range on default workers.
-func DefaultRange(Start, End int, Runner func(int)) error {
-	return defaultWorkers.Range(Start, End, Runner)
+// Range runs the range on default workers.
+func Range(start, end int, f func(int)) error {
+	return defaultWorkers.Range(start, end, f)
 }
 
 // Slice runs the slice on workers.
-func (w *Workers) Slice(Slice interface{}, Runner func(int, interface{})) error {
-	return runSlice(w.Max, Slice, Runner)
+func (w *Workers) Slice(i interface{}, f func(int, interface{})) error {
+	return runSlice(w.limit, i, f)
 }
 
 // Map runs the map on workers.
-func (w *Workers) Map(Map interface{}, Runner func(interface{}, interface{})) error {
-	return runMap(w.Max, Map, Runner)
+func (w *Workers) Map(i interface{}, f func(interface{}, interface{})) error {
+	return runMap(w.limit, i, f)
 }
 
 // Range runs the range on workers.
-func (w *Workers) Range(Start, End int, Runner func(int)) error {
-	return runRange(w.Max, Start, End, Runner)
+func (w *Workers) Range(start, end int, f func(int)) error {
+	return runRange(w.limit, start, end, f)
 }
 
-func runSlice(Limit int, Slice interface{}, Runner func(int, interface{})) error {
-	if reflect.TypeOf(Slice).Kind() != reflect.Slice {
-		return fmt.Errorf("Slice argument must be a slice")
+func runSlice(limit int, i interface{}, f func(int, interface{})) error {
+	if reflect.TypeOf(i).Kind() != reflect.Slice {
+		return fmt.Errorf("items must be a slice")
 	}
-	values := reflect.ValueOf(Slice)
-	if Limit <= 0 {
-		Limit = values.Len()
+
+	values := reflect.ValueOf(i)
+
+	if limit == 0 {
+		limit = NumCPU()
+	} else if limit < 0 {
+		limit = values.Len()
 	}
-	c := make(chan bool, Limit)
+
+	c := make(chan bool, limit)
 	for i := 0; i < values.Len(); i++ {
 		c <- true
 		go func(index int, value interface{}) {
 			defer func() { <-c }()
-			Runner(index, value)
+			f(index, value)
 		}(i, values.Index(i).Interface())
 	}
-	for i := 0; i < Limit; i++ {
+
+	for i := 0; i < limit; i++ {
 		c <- true
 	}
+
 	return nil
 }
 
-func runMap(Limit int, Map interface{}, Runner func(interface{}, interface{})) error {
-	if reflect.TypeOf(Map).Kind() != reflect.Map {
-		return fmt.Errorf("Map argument must be a map")
+func runMap(limit int, i interface{}, f func(interface{}, interface{})) error {
+	if reflect.TypeOf(i).Kind() != reflect.Map {
+		return fmt.Errorf("item must be a map")
 	}
-	value := reflect.ValueOf(Map)
-	if Limit <= 0 {
-		Limit = len(value.MapKeys())
+
+	value := reflect.ValueOf(i)
+
+	if limit == 0 {
+		limit = NumCPU()
+	} else if limit < 0 {
+		limit = len(value.MapKeys())
 	}
+
 	iter := value.MapRange()
-	c := make(chan bool, Limit)
+	c := make(chan bool, limit)
 	for iter.Next() {
 		k := iter.Key()
 		v := iter.Value()
 		c <- true
 		go func(k, v interface{}) {
 			defer func() { <-c }()
-			Runner(k, v)
+			f(k, v)
 		}(k.Interface(), v.Interface())
 	}
-	for i := 0; i < Limit; i++ {
+
+	for i := 0; i < limit; i++ {
 		c <- true
 	}
+
 	return nil
 }
 
-func runRange(Limit, Start, End int, Runner func(int)) error {
-	if Limit <= 0 {
-		Limit = End - Start + 1
+func runRange(limit, start, end int, f func(int)) error {
+	if limit == 0 {
+		limit = NumCPU()
+	} else if limit < 0 {
+		limit = end - start + 1
 	}
-	c := make(chan bool, Limit)
-	for i := Start; i <= End; i++ {
+
+	c := make(chan bool, limit)
+	for i := start; i <= end; i++ {
 		c <- true
 		go func(num int) {
 			defer func() { <-c }()
-			Runner(num)
+			f(num)
 		}(i)
 	}
-	for i := 0; i < Limit; i++ {
+
+	for i := 0; i < limit; i++ {
 		c <- true
 	}
+
 	return nil
 }
--- workers/workers_test.go
+++ workers/workers_test.go
@@ -3,7 +3,6 @@ package workers
 import (
 	"math/rand"
 	"reflect"
-	"runtime"
 	"sort"
 	"strings"
 	"sync"
@@ -17,27 +16,30 @@ func TestSlice(t *testing.T) {
 		times int
 	}
 	slice := []test{{"a", 1}, {"b", 2}, {"c", 3}}
+
 	result := make([]string, len(slice))
 	if err := Slice(slice, func(i int, item interface{}) {
 		result[i] = strings.Repeat(item.(test).char, item.(test).times)
 	}); err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
+
 	if !reflect.DeepEqual([]string{"a", "bb", "ccc"}, result) {
 		t.Errorf("expected %q; got %q", []string{"a", "bb", "ccc"}, result)
 	}
 }
 
 func TestMap(t *testing.T) {
-	m := &sync.Mutex{}
+	var m sync.Mutex
 	var result []string
 	if err := Map(map[string]int{"a": 1, "b": 2, "c": 3}, func(k, v interface{}) {
 		m.Lock()
 		result = append(result, strings.Repeat(k.(string), v.(int)))
 		m.Unlock()
 	}); err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
+
 	sort.Strings(result)
 	if !reflect.DeepEqual([]string{"a", "bb", "ccc"}, result) {
 		t.Errorf("expected %q; got %q", []string{"a", "bb", "ccc"}, result)
@@ -51,8 +53,9 @@ func TestRange(t *testing.T) {
 	if err := Range(1, end, func(num int) {
 		result[num-1] = strings.Repeat(items[num-1], num)
 	}); err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
+
 	if !reflect.DeepEqual([]string{"a", "bb", "ccc"}, result) {
 		t.Errorf("expected %q; got %q", []string{"a", "bb", "ccc"}, result)
 	}
@@ -60,46 +63,60 @@ func TestRange(t *testing.T) {
 
 func TestLimit(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
+
 	limit := rand.Intn(1000) + 51
-	var count1, count2, count3 int
+	workers := rand.Intn(50) + 1
+
+	var count1, count2, count3, count4 int
 	go Range(1, limit, func(_ int) {
 		count1++
 		for {
 			select {}
 		}
 	})
-	go DefaultRange(1, limit, func(_ int) {
+	go New(0).Range(1, limit, func(_ int) {
 		count2++
 		for {
 			select {}
 		}
 	})
-	workers := rand.Intn(50) + 1
-	go New(workers).Range(1, limit, func(_ int) {
+	go New(-1).Range(1, limit, func(_ int) {
 		count3++
 		for {
 			select {}
 		}
 	})
+	go New(workers).Range(1, limit, func(_ int) {
+		count4++
+		for {
+			select {}
+		}
+	})
+
 	time.Sleep(time.Second)
-	if count1 != limit {
-		t.Errorf("expected %d; got %d", limit, count1)
+
+	if count1 != NumCPU() {
+		t.Errorf("expected %d; got %d", NumCPU(), count1)
 	}
-	if count2 != runtime.NumCPU()*2 {
-		t.Errorf("expected %d; got %d", runtime.NumCPU()*2, count2)
+	if count2 != count1 {
+		t.Errorf("expected %d; got %d", count1, count2)
 	}
-	if count3 != workers {
-		t.Errorf("expected %d; got %d", workers, count3)
+	if count3 != limit {
+		t.Errorf("expected %d; got %d", limit, count3)
+	}
+	if count4 != workers {
+		t.Errorf("expected %d; got %d", workers, count4)
 	}
 }
 
-func TestSetMax(t *testing.T) {
-	SetMax(5)
-	if max := defaultWorkers.Max; max != 5 {
-		t.Errorf("expected %d; got %d", 5, max)
+func TestSetLimit(t *testing.T) {
+	SetLimit(5)
+	if limit := defaultWorkers.limit; limit != 5 {
+		t.Errorf("expected %d; got %d", 5, limit)
 	}
-	SetMax(100)
-	if max := defaultWorkers.Max; max != 100 {
-		t.Errorf("expected %d; got %d", 100, max)
+
+	SetLimit(100)
+	if limit := defaultWorkers.limit; limit != 100 {
+		t.Errorf("expected %d; got %d", 100, limit)
 	}
 }
