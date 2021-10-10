01a8b9f06ac308eb6b8b30bc86f0a0a817774e76
sunshineplan <sunshineplan@users.noreply.github.com>
2021-10-10 21:47:23 +0800

    httpsvr: Support TLS

--- httpsvr/httpsvr.go
+++ httpsvr/httpsvr.go
@@ -12,23 +12,21 @@ import (
 
 // Server defines parameters for running an HTTP server.
 type Server struct {
-	Unix    string
-	Host    string
-	Port    string
-	Handler http.Handler
+	*http.Server
+	Unix string
+	Host string
+	Port string
 }
 
 // Run runs an HTTP server which can be gracefully shut down.
-func (s *Server) Run() error {
-	server := &http.Server{Handler: s.Handler}
-
+func (s *Server) run(serve func(net.Listener) error) error {
 	idleConnsClosed := make(chan struct{})
 	go func() {
 		quit := make(chan os.Signal, 1)
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 		<-quit
 
-		if err := server.Shutdown(context.Background()); err != nil {
+		if err := s.Shutdown(context.Background()); err != nil {
 			fmt.Println("Failed to close server:", err)
 		}
 		close(idleConnsClosed)
@@ -43,14 +41,14 @@ func (s *Server) Run() error {
 		if err := os.Chmod(s.Unix, 0666); err != nil {
 			return fmt.Errorf("failed to chmod socket file: %v", err)
 		}
-		if err := server.Serve(listener); err != http.ErrServerClosed {
+		if err := serve(listener); err != http.ErrServerClosed {
 			return fmt.Errorf("failed to server: %v", err)
 		}
 	} else {
 		if s.Host != "" && s.Port != "" {
-			server.Addr = s.Host + ":" + s.Port
+			s.Addr = s.Host + ":" + s.Port
 		}
-		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		if err := serve(nil); err != http.ErrServerClosed {
 			return fmt.Errorf("failed to server: %v", err)
 		}
 	}
@@ -58,19 +56,45 @@ func (s *Server) Run() error {
 	return nil
 }
 
+// Run runs an HTTP server which can be gracefully shut down.
+func (s *Server) Run() error {
+	if s.Unix != "" {
+		return s.run(func(l net.Listener) error {
+			return s.Serve(l)
+		})
+	}
+	return s.run(func(_ net.Listener) error {
+		return s.ListenAndServe()
+	})
+}
+
+func (s *Server) RunTLS(certFile, keyFile string) error {
+	if s.Unix != "" {
+		return s.run(func(l net.Listener) error {
+			return s.ServeTLS(l, certFile, keyFile)
+		})
+	}
+	return s.run(func(_ net.Listener) error {
+		return s.ListenAndServeTLS(certFile, keyFile)
+	})
+}
+
 // TCP runs an HTTP server on TCP network listener.
-func TCP(hostport string, handler http.Handler) error {
-	if hostport == "" {
-		hostport = ":http"
-	}
-	host, port, err := net.SplitHostPort(hostport)
-	if err != nil {
-		return err
-	}
-	return (&Server{Host: host, Port: port, Handler: handler}).Run()
+func TCP(addr string, handler http.Handler) error {
+	return (&Server{Server: &http.Server{Addr: addr, Handler: handler}}).Run()
+}
+
+// TLS runs an HTTP server on TCP network listener and handle requests on incoming TLS connections.
+func TLS(addr string, handler http.Handler, certFile, keyFile string) error {
+	return (&Server{Server: &http.Server{Addr: addr, Handler: handler}}).RunTLS(certFile, keyFile)
 }
 
 // Unix runs an HTTP server on Unix domain socket listener.
 func Unix(unix string, handler http.Handler) error {
-	return (&Server{Unix: unix, Handler: handler}).Run()
+	return (&Server{Unix: unix, Server: &http.Server{Handler: handler}}).Run()
+}
+
+// UnixTLS runs an HTTP server on Unix domain socket listener and handle requests on incoming TLS connections.
+func UnixTLS(unix string, handler http.Handler, certFile, keyFile string) error {
+	return (&Server{Unix: unix, Server: &http.Server{Handler: handler}}).RunTLS(certFile, keyFile)
 }
