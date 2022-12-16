9b38deb4c60f2d37bd36cef5577021c44d9bc732
sunshineplan <sunshineplan@users.noreply.github.com>
2022-12-16 14:39:38 +0800

    loadbalance: Add RandomWeighted

--- loadbalance/random.go
+++ loadbalance/random.go
@@ -22,6 +22,19 @@ func Random[E any](items ...E) (LoadBalancer[E], error) {
 	return &random[E]{items: items}, nil
 }
 
+func RandomWeighted[E any](items ...Weighted[E]) (LoadBalancer[E], error) {
+	var pool []E
+	for _, i := range items {
+		for n := i.Weight; n > 0; n-- {
+			pool = append(pool, i.Item)
+		}
+	}
+	if len(pool) == 0 {
+		return nil, ErrEmptyLoadBalancer
+	}
+	return Random(pool...)
+}
+
 func (r *random[E]) Next() E {
 	return r.items[rand.Intn(len(r.items))]
 }
