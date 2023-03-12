68329ae3fc0fc6aec5d64382a0f736addc4d4f24
sunshineplan <sunshineplan@users.noreply.github.com>
2023-03-12 10:50:12 +0800

    log: Add flags const

--- log/logger.go
+++ log/logger.go
@@ -12,6 +12,17 @@ var (
 	_ Rotatable = (*Logger)(nil)
 )
 
+const (
+	Ldate         = 1 << iota     // the date in the local time zone: 2009/01/23
+	Ltime                         // the time in the local time zone: 01:23:23
+	Lmicroseconds                 // microsecond resolution: 01:23:23.123123.  assumes Ltime.
+	Llongfile                     // full file name and line number: /a/b/c/d.go:23
+	Lshortfile                    // final file name element and line number: d.go:23. overrides Llongfile
+	LUTC                          // if Ldate or Ltime is set, use UTC rather than the local time zone
+	Lmsgprefix                    // move the "prefix" from the beginning of the line to before the message
+	LstdFlags     = Ldate | Ltime // initial values for the standard logger
+)
+
 type Logger struct {
 	*log.Logger
 	m     sync.Mutex
@@ -42,30 +53,24 @@ func (l *Logger) setOutput(file *os.File, extra io.Writer) {
 	if l.file != nil && l.file != file {
 		l.file.Close()
 	}
+	l.file = file
+	l.extra = extra
 }
 
 func (l *Logger) SetOutput(file string, extra io.Writer) {
 	l.m.Lock()
 	defer l.m.Unlock()
-	f := openFile(file)
-	l.setOutput(f, extra)
-	l.file = f
-	l.extra = extra
+	l.setOutput(openFile(file), extra)
 }
 
 func (l *Logger) SetFile(file string) {
-	l.m.Lock()
-	defer l.m.Unlock()
-	f := openFile(file)
-	l.setOutput(f, l.extra)
-	l.file = f
+	l.SetOutput(file, l.extra)
 }
 
 func (l *Logger) SetExtra(extra io.Writer) {
 	l.m.Lock()
 	defer l.m.Unlock()
 	l.setOutput(l.file, extra)
-	l.extra = extra
 }
 
 func (l *Logger) Rotate() {
