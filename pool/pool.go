a60b34436b37d2c6cd719f636d81b5affddc5aa2
sunshineplan <sunshineplan@users.noreply.github.com>
2024-06-10 13:33:05 +0800

    Create pool package (#56)

---
+++ pool/pool.go
@@ -0,0 +1,19 @@
+package pool
+
+import "sync"
+
+type Pool[T any] struct {
+	p sync.Pool
+}
+
+func New[T any]() Pool[T] {
+	return Pool[T]{sync.Pool{New: func() any { return new(T) }}}
+}
+
+func (p *Pool[T]) Get() *T {
+	return p.p.Get().(*T)
+}
+
+func (p *Pool[T]) Put(x *T) {
+	p.p.Put(x)
+}
