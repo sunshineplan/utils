1a3d91bce881a4e4ee19eaccfb92e0e1a62b8e3e
sunshineplan <sunshineplan@users.noreply.github.com>
2024-06-13 13:14:25 +0800

    pool: make New return pointer (#58)

--- pool/pool.go
+++ pool/pool.go
@@ -3,11 +3,11 @@ package pool
 import "sync"
 
 type Pool[T any] struct {
-	p sync.Pool
+	p *sync.Pool
 }
 
-func New[T any]() Pool[T] {
-	return Pool[T]{sync.Pool{New: func() any { return new(T) }}}
+func New[T any]() *Pool[T] {
+	return &Pool[T]{&sync.Pool{New: func() any { return new(T) }}}
 }
 
 func (p *Pool[T]) Get() *T {
