1fb478eab2789a076e3ed93f6a098eea861f7cf7
sunshineplan <sunshineplan@users.noreply.github.com>
2024-07-02 16:57:27 +0800

    cache: Add List (#67)

---
+++ cache/list.go
@@ -0,0 +1,134 @@
+package cache
+
+import "container/list"
+
+// Element is an element of a linked list.
+type Element[T any] struct {
+	e *list.Element
+}
+
+// Value returns the value stored with this element.
+func (e *Element[T]) Value() T { return e.e.Value.(T) }
+
+// Next returns the next list element or nil.
+func (e *Element[T]) Next() *Element[T] {
+	if e := e.e.Next(); e != nil {
+		return &Element[T]{e}
+	}
+	return nil
+}
+
+// Prev returns the previous list element or nil.
+func (e *Element[T]) Prev() *Element[T] {
+	if e := e.e.Prev(); e != nil {
+		return &Element[T]{e}
+	}
+	return nil
+}
+
+// List represents a doubly linked list.
+// The zero value for List is an empty list ready to use.
+type List[T any] struct {
+	l list.List
+}
+
+// Init initializes or clears list l.
+func (l *List[T]) Init() *List[T] {
+	l.l.Init()
+	return l
+}
+
+// New returns an initialized list.
+func NewList[T any]() *List[T] { return new(List[T]).Init() }
+
+// Len returns the number of elements of list l. The complexity is O(1).
+func (l *List[T]) Len() int {
+	return l.l.Len()
+}
+
+// Front returns the first element of list l or nil if the list is empty.
+func (l *List[T]) Front() *Element[T] {
+	if e := l.l.Front(); e != nil {
+		return &Element[T]{e}
+	}
+	return nil
+}
+
+// Back returns the last element of list l or nil if the list is empty.
+func (l *List[T]) Back() *Element[T] {
+	if e := l.l.Back(); e != nil {
+		return &Element[T]{e}
+	}
+	return nil
+}
+
+// Remove removes e from l if e is an element of list l.
+// It returns the element value e.Value.
+// The element must not be nil.
+func (l *List[T]) Remove(e *Element[T]) T {
+	return l.l.Remove(e.e).(T)
+}
+
+// PushFront inserts a new element e with value v at the front of list l and returns e.
+func (l *List[T]) PushFront(v T) *Element[T] {
+	return &Element[T]{l.l.PushFront(v)}
+}
+
+// PushBack inserts a new element e with value v at the back of list l and returns e.
+func (l *List[T]) PushBack(v T) *Element[T] {
+	return &Element[T]{l.l.PushBack(v)}
+}
+
+// InsertBefore inserts a new element e with value v immediately before mark and returns e.
+// If mark is not an element of l, the list is not modified.
+// The mark must not be nil.
+func (l *List[T]) InsertBefore(v T, mark *Element[T]) *Element[T] {
+	return &Element[T]{l.l.InsertBefore(v, mark.e)}
+}
+
+// InsertAfter inserts a new element e with value v immediately after mark and returns e.
+// If mark is not an element of l, the list is not modified.
+// The mark must not be nil.
+func (l *List[T]) InsertAfter(v T, mark *Element[T]) *Element[T] {
+	return &Element[T]{l.l.InsertAfter(v, mark.e)}
+}
+
+// MoveToFront moves element e to the front of list l.
+// If e is not an element of l, the list is not modified.
+// The element must not be nil.
+func (l *List[T]) MoveToFront(e *Element[T]) {
+	l.l.MoveToFront(e.e)
+}
+
+// MoveToBack moves element e to the back of list l.
+// If e is not an element of l, the list is not modified.
+// The element must not be nil.
+func (l *List[T]) MoveToBack(e *Element[T]) {
+	l.l.MoveToBack(e.e)
+}
+
+// MoveBefore moves element e to its new position before mark.
+// If e or mark is not an element of l, or e == mark, the list is not modified.
+// The element and mark must not be nil.
+func (l *List[T]) MoveBefore(e, mark *Element[T]) {
+	l.l.MoveBefore(e.e, mark.e)
+}
+
+// MoveAfter moves element e to its new position after mark.
+// If e or mark is not an element of l, or e == mark, the list is not modified.
+// The element and mark must not be nil.
+func (l *List[T]) MoveAfter(e, mark *Element[T]) {
+	l.l.MoveAfter(e.e, mark.e)
+}
+
+// PushBackList inserts a copy of another list at the back of list l.
+// The lists l and other may be the same. They must not be nil.
+func (l *List[T]) PushBackList(other *List[T]) {
+	l.l.PushBackList(&other.l)
+}
+
+// PushFrontList inserts a copy of another list at the front of list l.
+// The lists l and other may be the same. They must not be nil.
+func (l *List[T]) PushFrontList(other *List[T]) {
+	l.l.PushFrontList(&other.l)
+}
