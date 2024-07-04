54b608cb96d964bf86c1aa51b4038c9e15125e22
sunshineplan <sunshineplan@users.noreply.github.com>
2024-07-04 13:07:03 +0800

    loadbalance: Use ring

--- loadbalance/roundrobin.go
+++ loadbalance/roundrobin.go
@@ -1,68 +1,90 @@
 package loadbalance
 
-import (
-	"sync/atomic"
-
-	"github.com/sunshineplan/utils/cache"
-)
+import "github.com/sunshineplan/utils/cache"
 
 var _ LoadBalancer[any] = &roundrobin[any]{}
 
-type roundrobin[E any] struct {
-	m    *cache.Map[[2]int64, *Weighted[E]]
-	n    int64
-	next atomic.Int64
-}
+type roundrobin[E any] cache.Ring[*E]
 
-func newRoundRobin[E any, Items []E | []*Weighted[E]](items Items) (*roundrobin[E], error) {
+func newRoundRobin[E any, Items []E | []Weighted[E]](items Items) *roundrobin[E] {
 	if len(items) == 0 {
-		return nil, ErrEmptyLoadBalancer
+		panic(ErrEmptyLoadBalancer)
 	}
-	var s []*Weighted[E]
+	var root *roundrobin[E]
 	switch items := any(items).(type) {
 	case []E:
 		for _, i := range items {
-			s = append(s, &Weighted[E]{i, 1})
+			r := cache.NewRing[*E](1)
+			r.Set(&i)
+			if root == nil {
+				root = (*roundrobin[E])(r)
+			} else {
+				root.Ring().Link(r)
+				root = (*roundrobin[E])(root.Ring().Next())
+			}
 		}
-	case []*Weighted[E]:
-		s = items
+		if root != nil {
+			root = (*roundrobin[E])(root.Ring().Next())
+		}
+	case []Weighted[E]:
+		for _, i := range items {
+			if i.Weight == 0 {
+				continue
+			}
+			item := &i.Item
+			r := cache.NewRing[*E](i.Weight)
+			for range r.Len() {
+				r.Set(item)
+				r = r.Next()
+			}
+			if root == nil {
+				root = (*roundrobin[E])(r)
+			} else {
+				root.Ring().Link(r)
+				root = (*roundrobin[E])(root.Ring().Next())
+			}
+		}
+		if root != nil {
+			root = (*roundrobin[E])(root.Ring().Next())
+		}
 	}
-	r := new(roundrobin[E])
-	r.m = new(cache.Map[[2]int64, *Weighted[E]])
-	for _, i := range s {
-		r.m.Store([2]int64{r.n, r.n + i.Weight}, i)
-		r.n += i.Weight
-	}
-	return r, nil
+	return root
 }
 
-func RoundRobin[E any](items ...E) (LoadBalancer[E], error) {
+func RoundRobin[E any](items ...E) LoadBalancer[E] {
 	return newRoundRobin[E](items)
 }
 
-func WeightedRoundRobin[E any](items ...*Weighted[E]) (LoadBalancer[E], error) {
+func WeightedRoundRobin[E any](items ...Weighted[E]) LoadBalancer[E] {
 	return newRoundRobin[E](items)
 }
 
-func (r *roundrobin[E]) get(n int64) (e E) {
-	if r.m == nil {
-		panic("load balancer is closed")
-	}
-	r.m.Range(func(i [2]int64, w *Weighted[E]) bool {
-		if n >= i[0] && n < i[1] {
-			e = w.Item
-			return false
-		}
-		return true
-	})
-	return
+func (r *roundrobin[E]) Len() int {
+	mu.RLock()
+	defer mu.RUnlock()
+	return r.Ring().Len()
 }
 
 func (r *roundrobin[E]) Next() (next E) {
-	return r.get(r.next.Swap((r.next.Load() + 1) % r.n))
-
+	mu.Lock()
+	defer mu.Unlock()
+	v := **r.Ring().Value()
+	*r = *(*roundrobin[E])(r.Ring().Next())
+	return v
 }
 
-func (r *roundrobin[E]) Close() {
-	r.m = nil
+func (r *roundrobin[E]) Ring() *cache.Ring[*E] {
+	return (*cache.Ring[*E])(r)
+}
+
+func (r *roundrobin[E]) Link(s LoadBalancer[E]) LoadBalancer[E] {
+	mu.Lock()
+	defer mu.Unlock()
+	return (*roundrobin[E])(r.Ring().Link(s.Ring()))
+}
+
+func (r *roundrobin[E]) Unlink(n int) LoadBalancer[E] {
+	mu.Lock()
+	defer mu.Unlock()
+	return (*roundrobin[E])(r.Ring().Unlink(n))
 }
